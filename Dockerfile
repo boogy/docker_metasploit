ae72ab5189d35db9809c961af766d424c08e9fb2
boogy <theboogymaster@gmail.com>
2017-08-07 09:43:46 +0200

    using tmux

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,21 @@ MAINTAINER boogy "linuxhackk@gmail.com"
 
 ENV DEBIAN_FRONTEND noninteractive
 
-ADD ./run.sh /run.sh
-
 # Install metasploit
 RUN grep "kali-rolling main contrib non-free" /etc/apt/sources.list \
     || echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
-RUN apt-get -y update ; apt-get -y dist-upgrade ; apt-get -y install \
+RUN apt-get -y update ; \
+    apt-get -y upgrade; \
+    apt-get -y dist-upgrade ; \
+    apt-get -y install \
     ruby \
     metasploit-framework \
     tmux \
     vim
 
+ADD ./run.sh /run.sh
+
 ENTRYPOINT ["/run.sh"]
+
 CMD ["start_msf"]
--- run.sh
+++ run.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
+SESSION=msf
+
+tmux -2 new-session -d -s $SESSION
+tmux select-pane -t 0
+tmux rename-window -t 0 'msfconsole'
+
+## Prevent tmux from complaining about locales
+sed -i "s/# fr_CH.UTF-8 UTF-8/fr_CH.UTF-8 UTF-8/g;s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+locale-gen
 
 if [ "$1" == "start_msf" ]; then
-
-    msfdb init
-    msfdb start
-
-    apt-get update
-    apt-get upgrade -y metasploit-framework
-
-    ## Prevent tmux from complaining about locales
-    export LANG=fr_CH.UTF-8
-    locale-gen fr_CH.UTF-8
-    localedef -f UTF-8 -i fr_CH fr_CH.UTF-8
-
-    # msfupdate
-    tmux new-session -d -s "msf" "msfconsole -L"
+    tmux send-keys "msfdb init ; msfdb start ; apt-get update ; apt-get upgrade -y metasploit-framework ; msfconsole" C-m
+    tmux attach -t $SESSION
 fi
 
-tmux attach
-exec "$@"
+tmux send-keys "exec $@" C-m
+tmux attach -t $SESSION
